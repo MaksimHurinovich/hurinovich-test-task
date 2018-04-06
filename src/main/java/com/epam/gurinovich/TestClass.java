aaaadb66ec56c4fa5bf229bae83ef7ea6f56ffe4
Maksim Hurinovich <Maksim_Hurinovich@epam.com>
2018-04-06 14:10:26 +0300

    extremely important function added

--- src/main/java/com/epam/gurinovich/TestClass.java
+++ src/main/java/com/epam/gurinovich/TestClass.java
@@ -9,7 +9,12 @@ public class TestClass {
         return a+b;
     }
 
+    private static float calculateDiff(float a, float b){
+        return b-a;
+    }
+
     public static void main(String[] args){
-        System.out.println(calculateSum(24, 'c'));
+        System.out.println(calculateSum(24.2f, 'c'));
+        System.out.print(calculateDiff('@', 024.5f));
     }
 }
