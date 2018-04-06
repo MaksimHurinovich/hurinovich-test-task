9e646ead043620c277e3048f08c1df66f1eb6375
Maksim Hurinovich <Maksim_Hurinovich@epam.com>
2018-04-06 14:39:30 +0300

    main class changed

--- src/main/java/com/epam/gurinovich/TestClass.java
+++ src/main/java/com/epam/gurinovich/TestClass.java
@@ -5,16 +5,12 @@ package com.epam.gurinovich;
  */
 public class TestClass {
 
-    private static float calculateSum(float a, float b){
+    public float calculateSum(float a, float b){
         return a+b;
     }
 
-    private static float calculateDiff(float a, float b){
+    public  float calculateDiff(float a, float b){
         return b-a;
     }
 
-    public static void main(String[] args){
-        System.out.println(calculateSum(24.2f, 'c'));
-        System.out.print(calculateDiff('@', 024.5f));
-    }
 }
