c097196a2deff6c593f0eb6a6445e26784144986
Maksim Hurinovich <Maksim_Hurinovich@epam.com>
2018-04-06 14:06:55 +0300

    example class created

---
+++ src/main/java/com/epam/gurinovich/TestClass.java
@@ -0,0 +1,15 @@
+package com.epam.gurinovich;
+
+/**
+ * Created by Maksim_Hurinovich on 4/6/2018.
+ */
+public class TestClass {
+
+    private static float calculateSum(float a, float b){
+        return a+b;
+    }
+
+    public static void main(String[] args){
+        System.out.println(calculateSum(24, 'c'));
+    }
+}
